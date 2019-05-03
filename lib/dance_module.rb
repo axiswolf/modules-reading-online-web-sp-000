c87ea342a416c0ff7f6fbdd04c790957de6e3248
Karen V <karenvo@usc.edu>
2019-05-03 08:57:51 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -2,4 +2,13 @@ module Dance
   def twirling
     "I'm twirling!"
   end
+  def jumping
+    "Look how high I'm jumping!"
+  end
+  def pirouette
+    "I'm doing a pirouette"
+  end
+  def take_a_bow
+    "Thank you, thankyou. It was a pleasure to dance for you all."
+  end
 end
